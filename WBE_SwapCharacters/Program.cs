f8c0325b5f012e18fc7b3a6b77aa0b41344b3555
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-11 23:08:24 -0400

    using recursion

--- WBE_SwapCharacters/Program.cs
+++ WBE_SwapCharacters/Program.cs
@@ -35,16 +35,7 @@ namespace WBE_SwapCharacters
 
         private static string SwapCharacters(string input)
         {
-            string result = "";
-            for (int i = 0; i < input.Length - 1; i += 2)
-            {
-                result +=  new string (input.Substring(i, 2).Reverse().ToArray());
-            }
-            if (input.Length % 2 == 1)
-            {
-                result += input[input.Length - 1];
-            }
-            return result;
+            return input.Length <= 1 ? input : new string (input.Substring(0, 2).Reverse().ToArray()) + SwapCharacters(input.Remove(0, 2));
         }
     }
 }
