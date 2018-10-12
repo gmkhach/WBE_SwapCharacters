53890019d0b85355890bbf1f48267cb797d64dd4
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-11 23:02:44 -0400

    eliminated the repetitive statements

--- WBE_SwapCharacters/Program.cs
+++ WBE_SwapCharacters/Program.cs
@@ -38,8 +38,7 @@ namespace WBE_SwapCharacters
             string result = "";
             for (int i = 0; i < input.Length - 1; i += 2)
             {
-                result += input[i + 1];
-                result += input[i];
+                result +=  new string (input.Substring(i, 2).Reverse().ToArray());
             }
             if (input.Length % 2 == 1)
             {
